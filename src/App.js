e3ca9f2d89a0f0f533c2c1c37ac8585dcfddde32
testt <test@test.com>
2023-10-26 23:35:46 -0400

    fully utilized sidebar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
+import * as React from 'react';
 import './App.css';
 
 import SideBar from './Component/SideBar';
 import TopHeader from './Component/TopHeader';
-import Body from './Component/Body';
+import Body from './Component/Body/Body';
 
 /*
 ## Main function that begins the App
 */
 function App() {
 
+  //Baseline State as a false state
+  const [side_bar, setSideBar] = React.useState({
+    left: false
+  });
 
-  
+  //Function to toggle the SideBar
+  const toggleSideBar = () => {
+    setSideBar({ left: !side_bar.left});
+  };
 
   return (
     <div className="App">
+      <TopHeader toggleSideBar={toggleSideBar} side_bar={side_bar.left}></TopHeader>
+      <SideBar open={side_bar.left}></SideBar>
     </div>
   );
 }
