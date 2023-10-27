b1cd7568e8b59c50c72659a74e0c2cb31798f68b
testt <test@test.com>
2023-10-26 23:59:55 -0400

    Added menu to the sidebars

--- src/Component/SideBar.js
+++ src/Component/SideBar.js
@@ -1,16 +1,17 @@
 import * as React from 'react';
 import Drawer from '@mui/material/Drawer';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { List } from '@mui/icons-material';
+import { List, ListItem, Divider } from "@mui/material";
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import SpeakerNotesIcon from '@mui/icons-material/SpeakerNotes';
+import RestaurantMenuIcon from '@mui/icons-material/RestaurantMenu';
 
-const testDrawer = createTheme({
+// Theme that will allow sidebar to remain below header
+const DrawerTheme = createTheme({
     components: {
-        // Name of the component
         MuiDrawer: {
             styleOverrides: {
-                // Name of the slot
                 root: {
-                    // Some CSS
                     position: "relative !important",
                     "& .MuiBackdrop-root": {
                         position: "relative !important"
@@ -34,17 +35,30 @@ const testDrawer = createTheme({
 export default function SideBar(props) {
 
 
+    const IconStyle = {
+        minWidth: '62px',
+        fontSize: 'xx-large'
+    }
 
     return (
         <React.Fragment>
-            <ThemeProvider theme={testDrawer}>
+            <ThemeProvider theme={DrawerTheme}>
                 <Drawer
                     anchor="left"
                     open={props.open}
-
                 >
                     <List>
-                       
+                        <ListItem>
+                            <SpeakerNotesIcon style={IconStyle}></SpeakerNotesIcon> My Story
+                        </ListItem>
+                        <Divider orientation="horizontal" variant="middle" style={{ backgroundColor: '#d8dce4' }} flexItem />
+                        <ListItem>
+                            <AssignmentIcon style={IconStyle}></AssignmentIcon> Todo List
+                        </ListItem>
+                        <Divider orientation="horizontal" variant="middle" style={{ backgroundColor: '#d8dce4' }} flexItem />
+                        <ListItem>
+                            <RestaurantMenuIcon style={IconStyle}></RestaurantMenuIcon> Wifes Recipes
+                        </ListItem>
                     </List>
                 </Drawer>
             </ThemeProvider>
